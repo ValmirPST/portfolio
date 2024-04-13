8c516493fe70c9c8da1f5b6cbc8e9324b8055e04
Valmir Paiva Stachin <valmirpaivacpm@gmail.com>
2024-04-13 08:27:58 -0300

    style header, heropage, about-me, projects

--- src/assets/css/style.js
+++ src/assets/css/style.js
@@ -1,5 +1,5 @@
-import './global.css';
-import './header.css';
-import './heropage.css';
-import './about-me.css';
-import './projects.css';
+import "./global.css";
+import "./header.css";
+import "./heropage.css";
+import "./about-me.css";
+import "./projects.css";
